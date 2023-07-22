2835bc3c1b0c4c8c3081c1d17cd150908f84f381
giacomotolari <tolarigiacomo@gmail.com>
2023-07-22 18:27:01 +0200

    create nav data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,40 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
+import { z } from "zod";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const navLinks = [
-  { name: "home", href: "/" },
-  { name: "dashboard", href: "/dashboard" },
-  { name: "projects", href: "/projects" },
-  { name: "skills", href: "/skills" },
-  { name: "companies", href: "/companies" },
-  { name: "about", href: "/about" },
-];
+const navDataSchema = z.object({
+  home: z.object({ name: z.literal("home"), href: z.literal("/") }),
+  dashboard: z.object({
+    name: z.literal("dashboard"),
+    href: z.literal("/dashboard"),
+  }),
+  projects: z.object({
+    name: z.literal("projects"),
+    href: z.literal("/projects"),
+  }),
+  skills: z.object({ name: z.literal("skills"), href: z.literal("/skills") }),
+  companies: z.object({
+    name: z.literal("companies"),
+    href: z.literal("/companies"),
+  }),
+  about: z.object({ name: z.literal("about"), href: z.literal("/about") }),
+});
+
+type NavData = z.infer<typeof navDataSchema>;
+
+const navData: NavData = {
+  home: { name: "home", href: "/" },
+  dashboard: { name: "dashboard", href: "/dashboard" },
+  projects: { name: "projects", href: "/projects" },
+  skills: { name: "skills", href: "/skills" },
+  companies: { name: "companies", href: "/companies" },
+  about: { name: "about", href: "/about" },
+};
+
+const navLinks = Object.values(navData);
 
 export const metadata: Metadata = {
   title: "giaco-dev",
