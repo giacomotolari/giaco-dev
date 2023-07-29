024f95dbe3056365d2ccef33a47d4f7d60396e21
giacomotolari <tolarigiacomo@gmail.com>
2023-07-29 16:32:49 +0200

    add img metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "giaco-dev",
   description: "Giacomo Tolari's portfolio",
+  openGraph: {
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1683009427479-c7e36bbb7bca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        width: 1200,
+        height: 630,
+        alt: "giaco.dev",
+      },
+    ],
+  },
 };
-
 export default function RootLayout({
   children,
 }: {
