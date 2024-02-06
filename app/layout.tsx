3d73f5b775379989e9ec7c3cc580183f72dfaa24
giacomotolari <tolarigiacomo@gmail.com>
2024-02-06 09:59:06 +0100

    test repos code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "@/src/components/UI/navbar/Navbar";
+import Navbar from "./src/components/UI/navbar/Navbar";
 import Providers from "./src/providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,17 +9,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "giaco-dev",
   description: "Giacomo Tolari's portfolio",
-  openGraph: {
-    images: [
-      {
-        url: "https://giaco.dev/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "giaco.dev",
-        type: "image/jpeg",
-      },
-    ],
-  },
+  // openGraph: {
+  //   images: [
+  //     {
+  //       url: "https://giaco.dev/og-image.jpg",
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "giaco.dev",
+  //       type: "image/jpeg",
+  //     },
+  //   ],
+  // },
 };
 
 export default function RootLayout({
--- app/src/components/HOC/WithWaitMounting.tsx
+++ app/src/components/HOC/WithWaitMounting.tsx
@@ -3,6 +3,15 @@ import { Skeleton } from "@/src/components/shadcn/ui/skeleton";
 
 type ComponentType<P> = FC<P>;
 
+/**
+ * Higher Order Component that adds a wait mounting behavior to a wrapped component.
+ * @template P - Props type of the wrapped component.
+ * @param {React.ComponentType<P>} WrappedComponent - The component to be wrapped.
+ * @param {React.ReactNode} [skeletonContent] - The content to be displayed while the component is mounting.
+ * @param {string} [className] - The CSS class name to be applied to the wrapper component.
+ * @param {boolean} [blur] - Whether to apply a blur effect to the skeleton content.
+ * @returns {React.FC<P>} - The wrapped component with wait mounting behavior.
+ */
 export default function WithWaitMounting<P extends object>(
   WrappedComponent: ComponentType<P>,
   skeletonContent?: ReactNode,
