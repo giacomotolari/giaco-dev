eae7bcd1bb1601183d512ff8204f7ddb4a1256b9
giacomotolari <tolarigiacomo@gmail.com>
2024-02-06 11:46:55 +0100

    feature: create basic projects layout

---
+++ app/projects/layout.tsx
@@ -0,0 +1,28 @@
+import Sidebar from "@/src/components/UI/sidebar/Sidebar";
+
+export default function ProjectsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const items = [
+    {
+      title: "All Projects",
+      href: "/projects",
+    },
+    {
+      title: "Personal",
+      href: "/projects/personal",
+    },
+    {
+      title: "Corporate",
+      href: "/projects/corporate",
+    },
+  ];
+  return (
+    <>
+      <Sidebar items={items} />
+      {children}
+    </>
+  );
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,3 +3,5 @@ export default function Projects() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
+
+
